f77e498177a2ffd24b7b5acbb48f811c32951580
Puginator5000 <emmaminime3662@gmail.com>
2024-09-27 23:03:47 -0700

    Done

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,44 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary", 3));
+    System.out.println(delEnd("library", 5));
+    System.out.println(comparyLower("framing", "framING"));
+    System.out.println(lengthFirst("Down is the new up"));
   }
 
-  // write solutions to problems below
+  public static String firstLastN(String word, int n) {
+    
+    // word = dictionary
+    // n = 3
+    // dicary
+    
+    String output;
+
+    //Getting first n letters
+    String firstN = word.substring(0, n);
+
+    //getting last n letters
+    int indexOfNToLast = word.length() - n;
+    String lastN = word.substring(indexOfNToLast);
+
+    output = firstN + lastN;
+    return output;
+   }
+
+   public static String delEnd(String word, int n) {
+    return word.substring(0, word.length() - n);
+   }
+
+   public static int comparyLower(String word1, String word2) {
+    word1 = word1.toLowerCase();
+    word2 = word2.toLowerCase();
+
+    return word1.compareTo(word2);
+   }
+
+   public static int lengthFirst(String sentence) { 
+    return sentence.indexOf(" ");
+   }
 
 }
